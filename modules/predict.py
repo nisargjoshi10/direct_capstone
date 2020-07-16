b23956a6c4fbf47c2aa7745246992bf0a2e9c8a6
Orion Dollar <oriondollar@Orions-MacBook-Pro-2.local>
2020-07-16 00:25:10 -0700

    predict.py

--- modules/predict.py
+++ modules/predict.py
@@ -49,7 +49,7 @@ predict_params = {'MAX_LENGTH': big_len,
                   'CHAR_WEIGHTS': char_weights_orgpl,
                   'PREDICT_PROPERTY': True}
 
-def predict_from_data(data, params, ckpt, n_splits, save_fn):
+def predict_from_data(data, params, ckpt, n_splits, target_code, save_fn):
 
     pvg = PlastVAEGen(params=params, name='{}_prop_predictor'.format(model_class))
     pvg.load(ckpt, transfer=False, predict_property=True)
@@ -64,28 +64,16 @@ def predict_from_data(data, params, ckpt, n_splits, save_fn):
         encoded_smiles[i,:] = encode_smiles(smi, pvg.params['MAX_LENGTH'], pvg.params['CHAR_DICT'])
 
     split_size = int(data.shape[0] / n_splits)
+    os.makedirs('predictions', exist_ok=True)
     for i in range(split_size):
         if i < split_size - 1:
             reconstructions, mu, logvar, predictions = pvg.predict(encoded_smiles[i*split_size:(i+1)*split_size,:], return_all=True)
-            print(reconstructions.shape, mu.shape, logvar.shape, predictions.shape)
         elif i == split_size - 1:
             reconstructions, mu, logvar, predictions = pvg.predict(encoded_smiles[i*split_size:,:], return_all=True)
-            print(reconstructions.shape, mu.shape, logvar.shape, predictions.shape)
-    # for i in range(10):
-    #     if i < 9:
-    #         org_reconstructions, org_mu, org_logvar, org_predictions = pvg.predict(org_encoded_smiles[i*5000:(i+1)*5000,:], return_all=True)
-    #     elif i == 9:
-    #         org_reconstructions, org_mu, org_logvar, org_predictions = pvg.predict(org_encoded_smiles[i*5000:,:], return_all=True)
-    #     #pl_reconstructions, pl_mu, pl_logvar, pl_predictions = pvg.predict(pl_encoded_smiles, return_all=True)
-    #
-    #     os.makedirs('predictions', exist_ok=True)
-    #     np.save('predictions/org_reconstructions_{}.npy'.format(i), org_reconstructions)
-    #     np.save('predictions/org_mu_{}.npy'.format(i), org_mu)
-    #     np.save('predictions/org_logvar_{}.npy'.format(i), org_logvar)
-    #     np.save('predictions/org_predictions_{}.npy'.format(i), org_predictions)
-    #     #np.save('predictions/pl_reconstructions.npy', pl_reconstructions)
-    #     #np.save('predictions/pl_mu.npy', pl_mu)
-    #     #np.save('predictions/pl_logvar.npy', pl_logvar)
-    #     #np.save('predictions/pl_predictions.npy', pl_predictions)
+    np.save('predictions/{}_reconstructions_{}_{}.npy'.format(target_code, save_fn, i), reconstructions)
+    np.save('predictions/{}_mu_{}_{}.npy'.format(target_code, save_fn, i), mu)
+    np.save('predictions/{}_logvar_{}_{}.npy'.format(target_code, save_fn, i), logvar)
+    np.save('predictions/{}_predictions_{}_{}.npy'.format(target_code, save_fn, i), predictions)
 
-predict_from_data(org_data, predict_params, 'checkpoints/latest_GRUGRU_prop_pred_epox_run2.ckpt', 10, 'org')
+predict_from_data(org_data, predict_params, 'checkpoints/best_GRUGRU_prop_pred_epox_run2.ckpt', 10, 'org', 'epox_run2')
+predict_from_data(pl_data, predict_params, 'checkpoints/best_GRUGRU_prop_pred_epox_run2.ckpt', 1, 'pl', 'epox_run2')
